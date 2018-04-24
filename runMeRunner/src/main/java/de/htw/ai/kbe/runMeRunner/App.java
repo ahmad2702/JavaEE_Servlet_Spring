fe129e9274caf19c21dd6c462a19aaf23106adcb
Akhmad Sadullaev <s.ahmad2702@gmail.com>
2018-04-24 14:31:32 +0200

    runMeRunner is created

---
+++ runMeRunner/src/main/java/de/htw/ai/kbe/runMeRunner/App.java
@@ -0,0 +1,13 @@
+package de.htw.ai.kbe.runMeRunner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
