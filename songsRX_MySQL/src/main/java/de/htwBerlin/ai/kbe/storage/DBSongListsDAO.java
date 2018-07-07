9a786f921881b185f2663fb71771aebb21491705
Akhmad Sadullaev <s.ahmad2702@gmail.com>
2018-07-07 21:58:28 +0200

    SongListsDAO with Interface are changed

--- songsRX_MySQL/src/main/java/de/htwBerlin/ai/kbe/storage/DBSongListsDAO.java
+++ songsRX_MySQL/src/main/java/de/htwBerlin/ai/kbe/storage/DBSongListsDAO.java
@@ -1,6 +1,8 @@
 package de.htwBerlin.ai.kbe.storage;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -10,6 +12,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
+import javax.ws.rs.core.GenericEntity;
 
 import de.htwBerlin.ai.kbe.data.SongLists;
 
@@ -37,35 +40,54 @@ public class DBSongListsDAO implements InterfaceSongListsDAO {
 	}
 
 	@Override
-	public Collection<SongLists> findAllSongLists(String id, boolean securityType) {
-		EntityManager entMan = entManFact.createEntityManager();
+	public GenericEntity<Collection<SongLists>> findAllSongLists(String userId, boolean securityType) {
+		GenericEntity<Collection<SongLists>> ret = null;
+		EntityManager em = entManFact.createEntityManager();
+		List<SongLists> allData = null;
 		try {
-
-			TypedQuery<SongLists> query = entMan.createQuery("SELECT c FROM SongLists c where user_id = " + "'" + id + "'",
-					SongLists.class);
-			if (securityType) {
-				query = entMan.createQuery("SELECT c FROM SongLists c where user_id = " + "'" + id + "' and securityType=1 ",
-						SongLists.class);
+			TypedQuery<SongLists> query = em.createQuery("select s from SongLists s", SongLists.class);
+			if (query.getResultList() != null && query.getResultList().size() > 0) {
+				allData = query.getResultList();
 			}
-			return query.getResultList();
 		} finally {
-			entMan.close();
+			em.close();
 		}
+		if (allData != null && allData.size() > 0) {
+			List<SongLists> selectedData = new ArrayList<SongLists>();
+			for (int i = 0; i < allData.size(); i++) {
+				if (allData.get(i).getUser().getUserId().equals(userId)) {
+					if (securityType) {
+						selectedData.add(allData.get(i));
+					} else if (allData.get(i).securityType()) {
+						selectedData.add(allData.get(i));
+					}
+				}
+			}
+			if (selectedData.size() > 0) {
+				ret = new GenericEntity<Collection<SongLists>>(selectedData) {};
+			}
+		}
+		return ret;
 	}
 
 	@Override
-	public SongLists findSongListById(String userId, Integer songListId, boolean securityType) {
+	public GenericEntity<SongLists> findSongListById(String userId, Integer songListId) {
+		GenericEntity<SongLists> ret = null;
 		EntityManager entMan = entManFact.createEntityManager();
+		
 		try {
-
-			TypedQuery<SongLists> query = entMan.createQuery(
-					"SELECT c FROM SongLists c where user_id = " + "'" + userId + "' and id=" + songListId,
-					SongLists.class);
-			if (securityType) {
-				query = entMan.createQuery("SELECT  c FROM SongLists c where user_id = " + "'" + userId
-						+ "' and securityType=1 and id=" + songListId, SongLists.class);
+			SongLists songLists = entMan.find(SongLists.class, songListId);
+			
+			if(songLists.getUser().getUserId().equals(userId)) {
+				
+			} else {
+				if(songLists.securityType()) {
+					ret = new GenericEntity<SongLists>(entMan.find(SongLists.class, songListId)) {};
+				}
 			}
-			return query.getSingleResult();
+		
+			return ret;
+			
 		} catch (NoResultException e) {
 			return null;
 		} finally {
@@ -94,29 +116,35 @@ public class DBSongListsDAO implements InterfaceSongListsDAO {
 	}
 
 	@Override
-	public boolean deleteSongLists(Integer id) {
+	public String deleteSongLists(String userId, Integer list_id) {
 		EntityManager entMan = entManFact.createEntityManager();
 		EntityTransaction entTransaction = entMan.getTransaction();
 		SongLists SongLists = null;
 		try {
-			SongLists = entMan.find(SongLists.class, id);
+			SongLists = entMan.find(SongLists.class, list_id);
+			
 			if (SongLists != null) {
-				System.out.println("Deleting: " + id);
-				entTransaction.begin();
-				entMan.remove(SongLists);
-				entTransaction.commit();
+				
+				System.out.println(userId + " -> " + SongLists.getUser().getUserId());
+				
+				if(SongLists.getUser().getUserId().equals(userId)) {
+					entTransaction.begin();
+					entMan.remove(SongLists);
+					entTransaction.commit();
+				} else {
+					return "forbidden";
+				}
 
+			} else {
+				return "not_found";
 			}
 		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Error removing SongLists: " + e.getMessage());
 			entTransaction.rollback();
-			return false;
-			// throw new PersistenceException("Could not remove entity: " + e.toString());
+			return "not_found";
 		} finally {
 			entMan.close();
 		}
-		return true;
+		return "ok";
 	}
 }
 
--- songsRX_MySQL/src/main/java/de/htwBerlin/ai/kbe/storage/InterfaceSongListsDAO.java
+++ songsRX_MySQL/src/main/java/de/htwBerlin/ai/kbe/storage/InterfaceSongListsDAO.java
@@ -2,17 +2,19 @@ package de.htwBerlin.ai.kbe.storage;
 
 import java.util.Collection;
 
+import javax.ws.rs.core.GenericEntity;
+
 import de.htwBerlin.ai.kbe.data.SongLists;
 
 public interface InterfaceSongListsDAO {
 
 	public SongLists findSongListsById(Integer id);
 
-	public Collection<SongLists> findAllSongLists(String id, boolean isPublic);
+	public GenericEntity<Collection<SongLists>> findAllSongLists(String id, boolean isPublic);
 
-	public SongLists findSongListById(String id, Integer songListId, boolean isPublic);
+	public GenericEntity<SongLists> findSongListById(String id, Integer songListId);
 	
 	public Integer saveSongLists(SongLists SongLists);
 
-	public boolean deleteSongLists(Integer id);
+	public String deleteSongLists(String userId, Integer list_id);
 }
